d934d1dd63ba4f3dd1b1d17ad1a9eb449b757a08
Ditmar <spyatorio@gmail.com>
2024-10-22 00:41:14 -0400

    Revert "modify icon and others (#121)" (#153)
    This reverts commit e31b8d14e2a435edafc25118bbd80572935fe3a8.
    
    Co-authored-by: Ditmar <ditmarcastro@gmail.com>

--- src/ui/components/atoms/tooltips_II/types/IProps.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export interface IProps {
-  children: React.ReactElement<{ className?: string }>;
-  variant?: 'top' | 'bottom' | 'left' | 'right';
-  text?: string;
-  size?: 'small' | 'medium' | 'large';
-}
