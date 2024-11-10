54e0acad7633d7492e6774c25d682d6e3a5b301c
Marco <narakucoronel@gmail.com>
2024-11-09 21:17:06 -0400

    Actualizar

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
-import './Toggle.css'; 
+import classNames from 'classnames';
+import './Toggle.css';
 
-const Toggle = ({ isOn, onToggle, labelOn = 'On', labelOff = 'Off', disabled, headerText }) => {
+function Toggle({ isOn, onToggle, labelOn = 'On', labelOff = 'Off', disabled, headerText }) {
+   const toggleClass = classNames('toggle-container', {
+    'disabled': disabled,
+  });
+
+  const labelClass = classNames('toggle-label', {
+    'on': isOn,
+    'off': !isOn,
+  });
+
   return (
     <div className="toggle-wrapper">
       {headerText && <div className="toggle-header">{headerText}</div>}
-      <div className={`toggle-container ${disabled ? 'disabled' : ''}`}>
-        <input 
-          type="checkbox" 
-          checked={isOn} 
-          onChange={onToggle} 
-          disabled={disabled}//true disabled
-          className="toggle-checkbox"
-        />
-        <span className="toggle-label">{isOn ? labelOn : labelOff}</span>
+      <div className={toggleClass}>
+        <label className="toggle-label-wrapper">
+          <input
+            type="checkbox"
+            checked={isOn}
+            onChange={onToggle}
+            disabled={disabled}
+            className="toggle-checkbox"
+            aria-checked={isOn}
+          />
+          <span className={labelClass}>{isOn ? labelOn : labelOff}</span>
+        </label>
       </div>
     </div>
   );
-};
+}
 
 export default Toggle;
