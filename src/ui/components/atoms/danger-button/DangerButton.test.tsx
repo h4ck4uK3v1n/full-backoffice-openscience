7fe545ed9e003fdb6a6f5b44085b6d02dcf1f71c
wendy <162728853+Mayrawendy@users.noreply.github.com>
2024-11-24 11:55:32 -0400

    cambios realizado (#201)

--- src/ui/components/atoms/danger-button/DangerButton.test.tsx
+++ src/ui/components/atoms/danger-button/DangerButton.test.tsx
@@ -3,27 +3,31 @@ import { render } from '@testing-library/react';
 import DangerButton from './DangerButton';
 
 describe('Testing DangerButton React Component', () => {
-  it('should render test 1', () => {
+  it('should render with variant "danger-primary"', () => {
     const { getByText } = render(<DangerButton variant="danger-primary">+</DangerButton>);
     const buttonText = getByText('+');
     expect(buttonText).toBeDefined();
   });
 
-  it('should render test 2', () => {
+  it('should render with variant "danger-secondary"', () => {
     const { getByText } = render(<DangerButton variant="danger-secondary">+</DangerButton>);
     const buttonText = getByText('+');
     expect(buttonText).toBeDefined();
   });
 
-  it('should render test 3', () => {
+  it('should render with variant "danger-tertiary"', () => {
     const { getByText } = render(<DangerButton variant="danger-tertiary">+</DangerButton>);
     const buttonText = getByText('+');
     expect(buttonText).toBeDefined();
   });
 
-  it('should render test 4', () => {
+  it('should render with variant "danger-disabled" and be disabled', () => {
     const { getByText } = render(<DangerButton variant="danger-disabled">+</DangerButton>);
     const buttonText = getByText('+');
     expect(buttonText).toBeDefined();
+
+    const buttonElement = buttonText.closest('button');
+    expect(buttonElement).not.toBeNull();
+    expect(buttonElement?.disabled).toBe(true); // Manually check if the button is disabled
   });
 });
